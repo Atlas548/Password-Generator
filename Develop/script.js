23e4d247ef735571df55f4d3820ac00af267dac9
Tyler Oates <oatesrotc548@gmail.com>
2022-07-23 15:52:14 -0700

    Working on adding variables, const, and functions to JS file

--- Develop/script.js
+++ Develop/script.js
@@ -1,76 +1,83 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var passArray = [];
-var numberChars = "0123456789";
-var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerChars = "abcdefghijklmnopqrstuvwxyz";
-var specialChars = "~!#$%&*-+|";
+// Arrays
+const numberChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const upperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const lowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const specialChars = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
-// function for prompts and confirms
-function specialChars() {
-  window.confirm(`do you want special characters?`);
-}
+// Statement Variables
+var passwordChar;
+var chooseLength = "";
+var passwordInput = [];
 
-function numberChars() {
-  window.confirm(`do you want numbers in your password?`);
-}
-
-function upperChars() {
-  window.confirm(`do you want upper characters?`);
-}
-
-function lowerChars() {
-  window.confirm(`Do you want lower case numbers`);
-}
-
-// Write password to the #password input
+// Writing password to the input 
 function writePassword() {
-
-  // Parameters of the generate password
-  function generatePassword() {
-
-    var passwordLength = window.prompt(`How many characters would you like in your password`);
-    
-    if (passwordLength < 7 || passwordLength > 100) {
-      window.alert(`Must be a minimum of 7 and less than 100 characters, try again`);
-      return;
-    
-    } if (!passwordLength) {
-      window.alert(`please enter how many characters you would like in your password`);
-      return;
-    
-    } if (numberChars()) {
-      passArray = passArray.concat(numberChars);
-      console.log(passArray);
-
-    } if (specialChars()) {
-      passArray = passArray.concat(specialChars);
-      console.log(passArray);
-
-    } if (lowerChars()) {
-      passArray = passArray.concat(lowerChars);
-      console.log(passArray);
-    
-    } if (upperChars()) {
-      passArray = passArray.concat(upperChars);
-      console.log(passArray);
-    
-    } console.log(passArray);
-  }
-
-  for (var i = 0; i == passwordLength; i++) {
-    var passRandom = Math.floor(Math.random() * passArray);
-  }
-
-
-
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Parameters of the generate password
+function generatePassword() {
+  var chooseLength = window.prompt("How many characters would you like your password? Choose between 8 and 128");
+  console.log(chooseLength);
+
+  if ((chooseLength < 8) || (chooseLength > 128)) {
+    alert("You must choose between a number between 8 and 128");
+    return;
+  }
+  
+  else {
+    var specialConfirm = confirm("Do you want special characters in your password?");
+    var numberConfirm = confirm("Do you want numbers in your password?");
+  }
+
+    // function for prompts and confirms
+    function specialConfirm() {
+    window.confirm(`do you want special characters?`);
+  }
+
+    function numberConfirm() {
+    window.confirm(`do you want numbers in your password?`);
+  }
+
+    function upperConfirm() {
+    window.confirm(`do you want upper characters?`);
+  }
+
+    function lowerConfirm() {
+    window.confirm(`Do you want lower case numbers`);
+  }
+
+
+     if (numberConfirm()) {
+      passArray = passArray.concat(numberChars);
+      console.log(passArray);
+
+    } if (specialConfirm()) {
+      passArray = passArray.concat(specialChars);
+      console.log(passArray);
+
+    } if (lowerConfirm()) {
+      passArray = passArray.concat(lowerChars);
+      console.log(passArray);
+    
+    } if (upperConfirm()) {
+      passArray = passArray.concat(upperChars);
+      console.log(passArray);
+    
+    }
+  }
+
+  for (var i = 0; i < writePassword; i++) {
+    var passRandom = Math.floor(Math.random() * passArray);
+    password.push(passRandom);
+  }
+
+
